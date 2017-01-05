376013c278b0f62d30b2e1136563cfe0cfafdbf9
Josh Pollock <Josh@JoshPress.net>
2017-01-05 00:22:58 -0500

    fix function names

---
+++ classes/interfaces/progress.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josh
+ * Date: 1/4/17
+ * Time: 7:28 PM
+ */
+
+namespace calderawp\conform;
+
+
+class progress {
+
+}
---
+++ classes/progress/cookie.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josh
+ * Date: 1/4/17
+ * Time: 7:32 PM
+ */
+
+namespace calderawp\conform\progress;
+
+
+class cookie {
+
+}
