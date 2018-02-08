e72d7a866c9adda777e45146b3d7e1d252b8d67d
Josh Pollock <josh@calderawp.com>
2018-02-08 09:44:42 -0500

    #69 add back old array keys

--- includes/calcdefaults.php
+++ includes/calcdefaults.php
@@ -42,9 +42,11 @@ class CF_Con_Form_CalcDefaults {
 				foreach ( $field[ 'config' ][ 'option' ] as $opt_id => $opt ) {
 					foreach (
 						array(
+						    'default',
 							'label',
 							'value',
-							'calc_value'
+							'calc_value',
+                            'calc_default'
 						) as $key
 					) {
 						if ( isset( $opt[ $key ] ) && self::is_prev_magic( $opt[ $key ] ) ) {
